104e3fb3432113fd3add9e44140a4c1cc9fdd2a3
Jun <j.zhao1@newcastle.ac.uk>
2023-10-23 12:01:30 +0100

    Completed challenge - passed tests

--- challenges-javascript/highAndLow.js
+++ challenges-javascript/highAndLow.js
@@ -4,7 +4,9 @@ function highAndLow(numbers) {
   for (let i = 0; i < lst.length; i++) {
     n.push(Number(lst[i]));
   }
-  return Math.max(...n), Math.min(...n);
+  let min = Math.min(...n);
+  let max = Math.max(...n);
+  return `${max} ${min}`;
 }
 
 console.log(highAndLow("1 2 3 4 5"));
