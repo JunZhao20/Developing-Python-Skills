25fd15ff21b67fb9cb420cf65f3f92bc792233ba
Jun <j.zhao1@newcastle.ac.uk>
2023-10-27 17:31:42 +0100

    completed challenge

--- challenges-javascript/3Or5.js
+++ challenges-javascript/3Or5.js
@@ -1,24 +1,33 @@
 function solution(number) {
   let num = 0;
   let lst = [];
+  let sum = 0;
   for (let i = 0; i < number; i++) {
     lst.push(num++);
   }
-  console.log(lst);
-  //   for (let x = 0; x < lst.length; x++)
-  //     if (lst[x] >= -1) {
-  //       const calc = lst.filter((n) => {
-  //         if (n % 3 == 0 || n % 5 == 0) {
-  //           return n;
-  //         }
-  //       });
-  //       // through each iteration it will add up within the accumulator
-  //       const sum = calc.reduce((accumulator, currentValue) => {
-  //         // this makes sue the currentValue is added up accumulator
-  //         return accumulator + currentValue;
-  //       });
-  //       return sum;
-  //     }
-  //   return 0;
+
+  for (let x = 0; x < lst.length; x++) {
+    if (lst[x] % 3 == 0 || lst[x] % 5 == 0) {
+      sum += lst[x];
+    }
+  }
+  return sum;
 }
-console.log(solution(2));
+
+//   for (let x = 0; x < lst.length; x++)
+//     if (lst[x] >= -1) {
+//       const calc = lst.filter((n) => {
+//         if (n % 3 == 0 || n % 5 == 0) {
+//           return n;
+//         }
+//       });
+//       // through each iteration it will add up within the accumulator
+//       const sum = calc.reduce((accumulator, currentValue) => {
+//         // this makes sue the currentValue is added up accumulator
+//         return accumulator + currentValue, 0;
+//       });
+//       return sum;
+//     }
+//   return 0;
+// }
+console.log(solution(10));
