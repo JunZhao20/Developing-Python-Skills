da878f8de19a48a7451882336fc278e20fb4dae0
Jun Zhao <junzhao1111@gmail.com>
2023-10-27 12:50:06 +0100

    commented algo

--- challenges-javascript/3Or5.js
+++ challenges-javascript/3Or5.js
@@ -4,21 +4,21 @@ function solution(number) {
   for (let i = 0; i < number; i++) {
     lst.push(num++);
   }
-
-  for (let x = 0; x < lst.length; x++)
-    if (lst[x] >= -1) {
-      const calc = lst.filter((n) => {
-        if (n % 3 == 0 || n % 5 == 0) {
-          return n;
-        }
-      });
-      // through each iteration it will add up within the accumulator
-      const sum = calc.reduce((accumulator, currentValue) => {
-        // this makes sue the currentValue is added up accumulator
-        return accumulator + currentValue;
-      });
-      return sum;
-    }
-  return 0;
+  console.log(lst);
+  //   for (let x = 0; x < lst.length; x++)
+  //     if (lst[x] >= -1) {
+  //       const calc = lst.filter((n) => {
+  //         if (n % 3 == 0 || n % 5 == 0) {
+  //           return n;
+  //         }
+  //       });
+  //       // through each iteration it will add up within the accumulator
+  //       const sum = calc.reduce((accumulator, currentValue) => {
+  //         // this makes sue the currentValue is added up accumulator
+  //         return accumulator + currentValue;
+  //       });
+  //       return sum;
+  //     }
+  //   return 0;
 }
-console.log(solution(1));
+console.log(solution(2));
