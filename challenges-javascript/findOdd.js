71be1a678f4b4f83d056e618023ee659a8ce4aec
Jun Zhao <junzhao1111@gmail.com>
2023-10-30 12:09:05 +0000

    Added new challenge, in progress

---
+++ challenges-javascript/findOdd.js
@@ -0,0 +1,9 @@
+function findOdd(A) {
+  // expand values in set obj into a list
+  const u = [...new Set(A)];
+  for (let i = 0; i < u.length; i++) {
+    let n = u[i];
+  }
+}
+
+findOdd([1, 1, 2]);
