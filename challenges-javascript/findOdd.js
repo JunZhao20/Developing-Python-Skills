1f2e74cd5530f46d9b50cdf0ae4a4ddb1e73d2fd
Jun <j.zhao1@newcastle.ac.uk>
2023-11-14 10:24:26 +0000

    reworking solution

--- challenges-javascript/findOdd.js
+++ challenges-javascript/findOdd.js
@@ -1,17 +1,12 @@
 function findOdd(A) {
   // UPDATE try adding count to obj
-  // let count = 0;
+  let count = 0;
   const u = [...new Set(A)];
   // const obj = new Set(A);
-  // for (let i = 0; i < u.length; i++) {
-  //   let n = u[i];
-  //   if
-  // }
-  let lst = [];
-  for (let n of A) {
-    if()
+  for (let i = 0; i < A.length; i++) {
+    if (A.includes(A[i])) {
+    }
   }
-  return lst;
 }
 
 console.log(findOdd([1, 1, 2]));
