ab3b1a9fb71cdd7060e95381d63c1d100f017fd6
Jun <j.zhao1@newcastle.ac.uk>
2023-11-25 16:41:34 +0000

    Fixing challenge bug

--- challenges-javascript/highestRank.js
+++ challenges-javascript/highestRank.js
@@ -9,13 +9,17 @@ function highestRank(arr) {
   const val = Object.values(dict);
   // sort to find max num
   const sorted = val.sort();
+  console.log(sorted);
   // grabs list of keys, looks for each key that has the highest value by comparing each key to the highest sorted value in sorted list
-  return Object.keys(dict).find(
-    (key) => dict[key] === sorted[sorted.length - 1]
+  return Number(
+    Object.keys(dict).find((key) => dict[key] === sorted[sorted.length - 1])
   );
 
   // for (let x = 0; x < dict.length; x++) { }
 }
 
-const res = highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]);
+const res = highestRank([
+  6, 13, 16, 18, 18, 11, 18, 15, 27, 24, 24, 17, 17, 17, 9, 11, 5, 11, 7, 13,
+  12,
+]);
 console.log(res);
